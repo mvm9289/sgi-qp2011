795b9b04f839601342d67abf2a1c240c3bc37a82
josenavasmolina@gmail.com <josenavasmolina@gmail.com@d8570aec-c9ed-2e7f-2de9-27051ed20347>
2011-05-20 08:00:36 +0000

    usamples

--- Bezier_Curves/bezier_curves.py
+++ Bezier_Curves/bezier_curves.py
@@ -39,24 +39,24 @@ vsamples = 20
 #]
 
 # baldufa
-#t = 2.0
-#q = t/4
-#points = [
-# [ 0, 0, 1],
-# [ t, 0, 0],
-# [ q, 0, 0],
-# [ 0, 0, -1]
-#]
+t = 2.0
+q = t/4
+points = [
+ [ 0, 0, 1],
+ [ t, 0, 0],
+ [ q, 0, 0],
+ [ 0, 0, -1]
+]
 
 # ou
-t = 1.3
-q = t/2
-points = [
- [ 0, 0, -1 ],
- [ t, 0, -1 ],
- [ q, 0, 1 ],
- [ 0, 0, 1 ],
-]
+#t = 1.3
+#q = t/2
+#points = [
+# [ 0, 0, -1 ],
+# [ t, 0, -1 ],
+# [ q, 0, 1 ],
+# [ 0, 0, 1 ],
+#]
 
 ############## BEZIER CURVES ##############
 
@@ -67,6 +67,11 @@ def function_u_coord(u, P1, P2, P3, P4):
 	fourth = u*u*u
 	return first*P1 + second*P2 + third*P3 + fourth*P4
 
+def function_u_v(u, v, P1, P2, P3, P4):
+	pointx = function_u_coord(u, P1[0], P2[0], P3[0], P4[0])
+	pointz = function_u_coord(u, P1[2], P2[2], P3[2], P4[2])
+	return [pointx * math.cos(v), pointx * math.sin(v), pointz]
+
 def bezier_curves():
 	#si tenemos dos curvas de bezier, la u va de 0 a 2; si 3, de 0 a 3 ...
 	#cuando pasamos del 1 hay que coger la segunda curva (ejemplo: esfera)
@@ -76,33 +81,66 @@ def bezier_curves():
 	
 	num_bezier_curves = len(points) / 4
 	
-	P1 = points[0]
-	P2 = points[1]
-	P3 = points[2]
-	P4 = points[3]
-	
-	control = 0
-	frontera = 1
-	indice = 4
-	u = 0
-	v = 0
-	
-	
-	while control < num_bezier_curves:
-		if control > frontera:
-			frontera = frontera + 1
-			P1 = points[indice]
-			P2 = points[indice+1]
-			P3 = points[indice+2]
-			P4 = points[indice+3]
-			u = u -1
-		u = u + incu
-		control = control + incu
-		print "Control: ", control, "u: ", u
+	v = 0.0
 
+	verts = []
+	faces = []
+	
+	samples = (usamples + num_bezier_curves - 1) / num_bezier_curves
+	
+	while v < 2.0*math.pi:
+		control = 0
+		frontera = 1.0
+		indice = 0
+		while control < num_bezier_curves:
+			P1 = points[indice]
+			P2 = points[indice + 1]
+			P3 = points[indice + 2]
+			P4 = points[indice + 3]
+			u = 0.0
+			#while u < 1.0:
+			#	point = function_u_v(u, v, P1, P2, P3, P4)
+			#	verts.append(point)
+			#	u = u + incu
+			for i in range(samples):
+				point = function_u_v(u, v, P1, P2, P3, P4)
+				verts.append(point)
+				u = u + incu
+			u = 1.0
+			point = function_u_v(u, v, P1, P2, P3, P4)
+			verts.append(point)
+			control = control + 1
+			indice = indice + 4
+			
+		v = v + incv
+	
+		
+	# P[0..usamples-1]
+	# Q[0..usamples-1]
+	P = []
+	Q = []
+	for i in range(usamples):
+		P.append(i)
+		Q.append(i)
+	
+	for i in range(1, vsamples):
+		A = usamples * i
+		for j in range(1, usamples):
+			B = usamples * i + j
+			faces.append([A, B, P[j], P[j - 1]])
+			P[j - 1] = A
+			A = B
+		P[usamples-1] = B
+	
+	for i in range(1, usamples):
+		faces.append([Q[i - 1], Q[i], P[i], P[i-1]])
+	
+	BPyAddMesh.add_mesh_simple('MC', verts, [], faces)
+	print "Vert: ", verts[usamples-1]
 
 def main():
 	bezier_curves()
-
+	Window.EditMode(0)
+	Window.WaitCursor(1)
 
 main()
