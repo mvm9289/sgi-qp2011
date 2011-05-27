f20f5862773bd536967d09e1ba33dcbc98efe296
mvm9289 <mvm9289@d8570aec-c9ed-2e7f-2de9-27051ed20347>
2011-05-27 07:57:28 +0000

    Falta curvatures

--- trunk/Bezier_Curves/bezier_curves.py
+++ trunk/Bezier_Curves/bezier_curves.py
@@ -14,7 +14,6 @@ Group:
 
 import BPyAddMesh
 import Blender
-from taulaMC import lut
 from Blender import Scene, Mesh, Window, sys
 import BPyMessages
 import bpy
@@ -39,24 +38,24 @@ vsamples = 20
 #]
 
 # baldufa
-t = 2.0
-q = t/4
-points = [
- [ 0, 0, 1],
- [ t, 0, 0],
- [ q, 0, 0],
- [ 0, 0, -1]
-]
+#t = 2.0
+#q = t/4
+#points = [
+# [ 0, 0, 1],
+# [ t, 0, 0],
+# [ q, 0, 0],
+# [ 0, 0, -1]
+#]
 
 # ou
-#t = 1.3
-#q = t/2
-#points = [
-# [ 0, 0, -1 ],
-# [ t, 0, -1 ],
-# [ q, 0, 1 ],
-# [ 0, 0, 1 ],
-#]
+t = 1.3
+q = t/2
+points = [
+ [ 0, 0, -1 ],
+ [ t, 0, -1 ],
+ [ q, 0, 1 ],
+ [ 0, 0, 1 ],
+]
 
 ############## BEZIER CURVES ##############
 
@@ -76,7 +75,7 @@ def bezier_curves():
 	#si tenemos dos curvas de bezier, la u va de 0 a 2; si 3, de 0 a 3 ...
 	#cuando pasamos del 1 hay que coger la segunda curva (ejemplo: esfera)
 	
-	incu = ( float(len(points)) / 4.0) / float(usamples)
+	incu = ( float(len(points)) / 4.0) / float(usamples - 1)
 	incv = (2.0*math.pi) / float(vsamples)
 	
 	num_bezier_curves = len(points) / 4
@@ -86,29 +85,33 @@ def bezier_curves():
 	verts = []
 	faces = []
 	
-	samples = (usamples + num_bezier_curves - 1) / num_bezier_curves
+	#samples = (usamples + num_bezier_curves - 1) / num_bezier_curves
 	
 	while v < 2.0*math.pi:
 		control = 0
 		frontera = 1.0
 		indice = 0
+		u = 0.0
 		while control < num_bezier_curves:
 			P1 = points[indice]
 			P2 = points[indice + 1]
 			P3 = points[indice + 2]
 			P4 = points[indice + 3]
-			u = 0.0
-			#while u < 1.0:
-			#	point = function_u_v(u, v, P1, P2, P3, P4)
-			#	verts.append(point)
-			#	u = u + incu
-			for i in range(samples):
+			#u = 0.0
+			while u < 1.0:
 				point = function_u_v(u, v, P1, P2, P3, P4)
 				verts.append(point)
 				u = u + incu
-			u = 1.0
-			point = function_u_v(u, v, P1, P2, P3, P4)
-			verts.append(point)
+			#for i in range(samples):
+			#	point = function_u_v(u, v, P1, P2, P3, P4)
+			#	verts.append(point)
+			#	u = u + incu
+			if control == num_bezier_curves - 1:
+				u = 1.0
+				point = function_u_v(u, v, P1, P2, P3, P4)
+				verts.append(point)
+			else:
+				u = u - 1.0
 			control = control + 1
 			indice = indice + 4
 			
@@ -119,28 +122,103 @@ def bezier_curves():
 	# Q[0..usamples-1]
 	P = []
 	Q = []
-	for i in range(usamples):
+	for i in range(usamples + 1):
 		P.append(i)
 		Q.append(i)
 	
+	v_e = []
+	v_f = []
+	for i in range(len(verts)):
+		v_e.append([])
+		v_f.append([])
+	
 	for i in range(1, vsamples):
-		A = usamples * i
-		for j in range(1, usamples):
-			B = usamples * i + j
+		A = (usamples + 1) * i
+		for j in range(1, usamples + 1):
+			B = (usamples + 1) * i + j
 			faces.append([A, B, P[j], P[j - 1]])
+			
+			v_f[A].append(len(faces) - 1)
+			v_f[B].append(len(faces) - 1)
+			v_f[P[j]].append(len(faces) - 1)
+			v_f[P[j - 1]].append(len(faces) - 1)
+			
+			v_e[A].append(B)
+			v_e[A].append(P[j - 1])
+			v_e[B].append(A)
+			v_e[B].append(P[j])
+			v_e[P[j]].append(B)
+			v_e[P[j]].append(P[j - 1])
+			v_e[P[j - 1]].append(A)
+			v_e[P[j - 1]].append(P[j])
+			
 			P[j - 1] = A
 			A = B
-		P[usamples-1] = B
+		P[usamples] = B
 	
-	for i in range(1, usamples):
+	for i in range(1, usamples + 1):
 		faces.append([Q[i - 1], Q[i], P[i], P[i-1]])
+		
+		v_f[Q[i - 1]].append(len(faces) - 1)
+		v_f[Q[i]].append(len(faces) - 1)
+		v_f[P[i]].append(len(faces) - 1)
+		v_f[P[i - 1]].append(len(faces) - 1)
+			
+		v_e[Q[i - 1]].append(Q[i])
+		v_e[Q[i - 1]].append(P[i - 1])
+		v_e[Q[i]].append(Q[i - 1])
+		v_e[Q[i]].append(P[i])
+		v_e[P[i]].append(Q[i])
+		v_e[P[i]].append(P[i - 1])
+		v_e[P[i - 1]].append(Q[i - 1])
+		v_e[P[i - 1]].append(P[i])
 	
 	BPyAddMesh.add_mesh_simple('MC', verts, [], faces)
-	print "Vert: ", verts[usamples-1]
+	
+	return [v_e, v_f]
+
+def compute_curvature(v_e, v_f):
+	# Obtain active object
+	ob = bpy.data.scenes.active.objects.active
+	
+	# Check thath is a mesh
+	if not ob or ob.type != 'Mesh':
+		BPyMessages.Error_NoMeshActive()
+		return
+
+	# Get mesh
+	mesh = ob.getData(mesh = 1)
+	
+	curv = []
+	for v in mesh.verts:
+		# Compute sum angles
+		alpha = 0.0
+		for i in range(0, len(v_e[v.index]), 2):
+			v1 = mesh.verts[v_e[v.index][i]].co - v.co
+			v2 = mesh.verts[v_e[v.index][i + 1]].co - v.co
+			v1.normalize()
+			v2.normalize()
+			dotProd = v1.dot(v2)
+			alpha = alpha + math.acos(dotProd)
+		print "V: ", v.index, " alpha: ", alpha
+	
+	mesh.vertexColors= True # Enable face, vertex colors
+	for f in mesh.faces:
+		for i, v in enumerate(f):
+			no= v.no
+			col= f.col[i]
+			col.r= int((no.x+1)*128)
+			col.g= int((no.y+1)*128)
+			col.b= int((no.z+1)*128)
+
 
 def main():
-	bezier_curves()
 	Window.EditMode(0)
 	Window.WaitCursor(1)
+	
+	result = bezier_curves()
+	compute_curvature(result[0], result[1])
+	
+	Window.WaitCursor(0) 
 
 main()
